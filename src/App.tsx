e5d7afbb48fe227a0d8217b9c276eddbdcedc4cc
Italo Costa <costa.italo.ti@gmail.com>
2023-09-22 13:26:31 -0300

    Structured project

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { Outlet } from "react-router-dom"
 
 export const App = () => {
     return (
-      <div className="container h-screen w-screen mx-auto justify-items-center align-middle justify-center text-red-500 mt-10 flex-col">
-        <header>
+      <div className="container flex-auto flex-col h-screen w-screen mx-auto 
+       text-red-500 mt-10 justify-center ">
+        <header className="flex  justify-center">
         header
         </header>
-        <h1 className="text-7xl">CineSeriesHub</h1>
+        <h1 className="flex  justify-center text-7xl">CineSeriesHub</h1>
         <Outlet />
-        <footer>
+        <footer className="flex  justify-center">
             footer
         </footer>
       </div>
